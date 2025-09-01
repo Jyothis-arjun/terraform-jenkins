9c6dbb5de472277e4854e1130826d298d668d700
Jyothis-arjun <sjyothi1829@gmail.com>
2025-09-01 13:51:47 +0530

    Update main.tf

--- EC2/main.tf
+++ EC2/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "terraform-instance-1" {
   ami           = "ami-0861f4e788f5069dd"
   instance_type = "t2.micro"
-  key_name      = "demo-2025"
+  key_name      = "tc-pem"
   tags = {
     Name  = "ss-instance-1"
     appid = "345"
